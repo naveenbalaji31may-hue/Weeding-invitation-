13f4bcdbc091cd57d49eeff8946c5411173ef2a2
naveenbalaji31may-hue <naveenbalaji31may@gmail.com>
2026-07-31 09:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-    alert("Thank you! We look forward to celebrating with you. ❤️");
+for(let i=0;i<80;i++){
+    let petal=document.createElement("div");
+    petal.className="petal";
+    petal.innerHTML="🌹";
+    petal.style.left=Math.random()*100+"vw";
+    petal.style.fontSize=(15+Math.random()*25)+"px";
+    petal.style.animationDuration=(4+Math.random()*6)+"s";
+    petal.style.animationDelay=Math.random()*6+"s";
+    document.body.appendChild(petal);
 }
